93933d9c0d91624afafeed869fab4234564d8cef
Filip Madzia <filipmadzia33@gmail.com>
2025-11-04 21:57:03 +0100

    Empty maui project

---
+++ Sudoku.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Sudoku.Mobile
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Sudoku.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sudoku.Mobile
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
